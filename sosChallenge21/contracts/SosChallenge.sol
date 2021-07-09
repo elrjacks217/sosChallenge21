079e375e724c7679da9381590398f183bd15cf17
elrjacks217 <77869750+elrjacks217@users.noreply.github.com>
2021-07-09 13:54:20 -0400

    Update SosChallenge.sol

--- sosChallenge21/contracts/SosChallenge.sol
+++ sosChallenge21/contracts/SosChallenge.sol
@@ -3,4 +3,15 @@ pragma solidity ^0.5.16;
 contract SosChallenge {
   uint public count = 0;
   
+  struct Profile{
+    unit id;
+    string content;
+    bool updated;
+  }
+  
+  mapping(unit => Profile) public profiles;
+  
+  function createProfile(string memore _content) public {
+  
+  }
 }
